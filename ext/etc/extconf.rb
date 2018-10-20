f1fad5e4ae6e5eadd4d7a9a9d9e8868f18acbef8
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-10-20 19:26:59 +0900

    Prefer to use File.exist?

--- ext/etc/extconf.rb
+++ ext/etc/extconf.rb
@@ -43,7 +43,7 @@ have_struct_member('struct group', 'gr_passwd', 'grp.h')
 
 # for https://github.com/ruby/etc
 srcdir = File.expand_path("..", __FILE__)
-if !File.exists?("#{srcdir}/depend")
+if !File.exist?("#{srcdir}/depend")
   %x[#{RbConfig.ruby} #{srcdir}/mkconstants.rb -o #{srcdir}/constdefs.h]
 end
 
