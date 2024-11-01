7f12b05c423d98ce7b8a798bdf15c606ddbed165
Nobuyoshi Nakada <nobu@ruby-lang.org>
2024-11-01 17:36:16 +0900

    Add CI on Windows

--- ext/etc/extconf.rb
+++ ext/etc/extconf.rb
@@ -21,28 +21,8 @@ have_func("sysconf")
 have_func("confstr")
 have_func("fpathconf")
 
-have_struct_member('struct passwd', 'pw_gecos', 'pwd.h')
-have_struct_member('struct passwd', 'pw_change', 'pwd.h')
-have_struct_member('struct passwd', 'pw_quota', 'pwd.h')
-if have_struct_member('struct passwd', 'pw_age', 'pwd.h')
-  case what_type?('struct passwd', 'pw_age', 'pwd.h')
-  when "string"
-    f = "safe_setup_str"
-  when "long long"
-    f = "LL2NUM"
-  else
-    f = "INT2NUM"
-  end
-  $defs.push("-DPW_AGE2VAL="+f)
-end
-have_struct_member('struct passwd', 'pw_class', 'pwd.h')
-have_struct_member('struct passwd', 'pw_comment', 'pwd.h') unless /cygwin/ === RUBY_PLATFORM
-have_struct_member('struct passwd', 'pw_expire', 'pwd.h')
-have_struct_member('struct passwd', 'pw_passwd', 'pwd.h')
-have_struct_member('struct group', 'gr_passwd', 'grp.h')
-
 # for https://github.com/ruby/etc
-srcdir = File.expand_path("..", __FILE__)
+srcdir = __dir__
 constdefs = "#{srcdir}/constdefs.h"
 if !File.exist?(constdefs)
   ruby = RbConfig.ruby
